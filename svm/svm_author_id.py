fdbe7e42786786cb5ed3d87c3fc2c09de665d016
Markus Shepherd <markus.r.shepherd@gmail.com>
2016-10-08 14:04:07 +0300

    changes for decision_tree, regression, svm, tools

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -18,8 +18,8 @@ from email_preprocess import preprocess
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-
-
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
 
 
 #########################################################
@@ -27,4 +27,13 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 
-
+from sklearn.svm import SVC
+clf = SVC(kernel='rbf', C=10000)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+from sklearn.metrics import accuracy_score
+print(accuracy_score(labels_test, pred))
+print(pred[10])
+print(pred[26])
+print(pred[50])
+print(sum(pred))
